52661de487f670a40f584dfff0fbe850c4f96cf5
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-09-26 23:10:59 -0300

    initial structure

---
+++ src/app/features/map/components/layer-control/layer-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layer-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './layer-control.component.html',
+  styleUrl: './layer-control.component.scss'
+})
+export class LayerControlComponent {
+
+}
