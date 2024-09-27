9d11726f1354eb8da9569127fcd9e93cc0d2a6a6
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-09-26 23:30:37 -0300

    Initial components implementation

--- src/app/core/services/map.service.ts
+++ src/app/core/services/map.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import * as d3 from 'd3';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,11 @@ import { Injectable } from '@angular/core';
 export class MapService {
 
   constructor() { }
+
+  getMapData(): Observable<any> {
+    // For now, return a mock object
+    return of(/*mock map data*/);
+  }
+
+  // More map methods will be added here
 }
--- src/app/features/map/components/map/map.component.ts
+++ src/app/features/map/components/map/map.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import * as d3 from 'd3';
+import { MapService } from '../../../../core/services/map.service';
 
 @Component({
   selector: 'app-map',
-  standalone: true,
-  imports: [],
-  templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  template: '<div #mapContainer></div>',
+  styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  @ViewChild('mapContainer', { static: true }) private mapContainer!: ElementRef;
 
+  private svg: any;
+  private projection: any;
+  private path: any;
+
+  constructor(private mapService: MapService) {}
+
+  ngOnInit() {
+    this.initializeMap();
+  }
+
+  private initializeMap(): void {
+    const width = 800;
+    const height = 600;
+
+    this.svg = d3.select(this.mapContainer.nativeElement)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    this.projection = d3.geoMercator()
+      .center([12.572149, 42.674749])
+      .scale(1800)
+      .translate([width / 2, height / 2]);
+
+    this.path = d3.geoPath().projection(this.projection);
+
+    // Load and display map data
+    this.mapService.getMapData().subscribe(data => {
+      this.svg.selectAll('path')
+        .data(data.features)
+        .enter()
+        .append('path')
+        .attr('d', this.path)
+        .attr('fill', 'none')
+        .attr('stroke', '#333')
+        .attr('stroke-width', 1);
+    });
+  }
 }
